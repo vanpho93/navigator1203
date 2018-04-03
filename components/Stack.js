2a90fb72216406c3b9985c1624db72624852e52c
Van Pho Nguyen <vanpho02@gmail.com>
2018-04-03 15:33:18 +0700

    stack

--- components/Stack.js
+++ components/Stack.js
@@ -1,12 +1,64 @@
 import React, { Component } from 'react';
-import { View, TouchableOpacity, Text } from 'react-native';
+import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
 
 export class Stack extends Component {
     render() {
+        return <Account />;
+    }
+}
+
+class SignIn extends Component {
+    render() {
         return (
-            <View>
-                <Text>Stack Component</Text>
+            <View style={styles.signInContainer}>
+                <Text style={styles.titleText}>SignIn Component</Text>
+                <TouchableOpacity style={styles.buttonContainer}>
+                    <Text style={styles.buttonText}>Sign In</Text>
+                </TouchableOpacity>
             </View>
         );
     }
 }
+
+class Account extends Component {
+    render() {
+        return (
+            <View style={styles.accountContainer}>
+                <Text style={styles.titleText}>Account Component</Text>
+                <TouchableOpacity style={styles.buttonContainer}>
+                    <Text style={styles.buttonText}>Sign Out</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    signInContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#7A609C'
+    },
+    accountContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#EF4F21'
+    },
+    buttonContainer: {
+        backgroundColor: '#117ACC',
+        padding: 10,
+        margin: 10,
+        borderRadius: 5,
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: 'bold'
+    },
+    titleText: {
+        fontSize: 20,
+        color: 'white',
+        fontWeight: 'bold'
+    }
+});
