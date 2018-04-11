14fa5157d21d3ad76492847151e07c6e345b5c8b
Van Pho Nguyen <vanpho02@gmail.com>
2018-04-11 16:21:56 +0700

    can upload

--- components/camera/Camera.js
+++ components/camera/Camera.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { View, TouchableOpacity, Text, StyleSheet, Image } from 'react-native';
+import RNFetchBlob from 'react-native-fetch-blob';
+
 import defaultImage from '../../images/default.png';
 import { openPicker } from './api/openPicker';
 
@@ -8,6 +10,7 @@ export class Camera extends Component {
         super(props);
         this.state = { imageSource: null };
         this.showPicker = this.showPicker.bind(this);
+        this.uploadImage = this.uploadImage.bind(this);
     }
     render() {
         const { imageSource } = this.state;
@@ -20,6 +23,9 @@ export class Camera extends Component {
                 <TouchableOpacity style={styles.buttonContainer} onPress={this.showPicker}>
                     <Text style={styles.buttonText}>Get Image</Text>
                 </TouchableOpacity>
+                <TouchableOpacity style={styles.buttonContainer} onPress={this.uploadImage}>
+                    <Text style={styles.buttonText}>Upload to server</Text>
+                </TouchableOpacity>
             </View>
         );
     }
@@ -27,28 +33,41 @@ export class Camera extends Component {
     showPicker() {
         openPicker(source => this.setState({ imageSource: source }));
     }
+
+    uploadImage() {
+        const options = {
+            Authorization: "Bearer access-token",
+            otherHeader: "foo",
+            'Content-Type': 'multipart/form-data',
+        };
+        const URL = 'http://www.example.com/upload-form';
+        const data = [{ name: 'name', data: 'user' }];
+        RNFetchBlob.fetch('POST', URL, options, data)
+        .then(response => alert(JSON.stringify(response)))
+        .catch(error => alert('Error'));
+    }
 }
 
-    const styles = StyleSheet.create({
-        cameraContainer: {
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center',
-            backgroundColor: '#7A609C'
-        },
-        buttonContainer: {
-            backgroundColor: '#117ACC',
-            padding: 10,
-            margin: 10,
-            borderRadius: 5,
-        },
-        buttonText: {
-            color: 'white',
-            fontWeight: 'bold'
-        },
-        titleText: {
-            fontSize: 20,
-            color: 'white',
-            fontWeight: 'bold'
-        }
-    });
+const styles = StyleSheet.create({
+    cameraContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#7A609C'
+    },
+    buttonContainer: {
+        backgroundColor: '#117ACC',
+        padding: 10,
+        margin: 10,
+        borderRadius: 5,
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: 'bold'
+    },
+    titleText: {
+        fontSize: 20,
+        color: 'white',
+        fontWeight: 'bold'
+    }
+});
